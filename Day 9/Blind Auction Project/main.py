18de688deab1e4ef6bb35dce31d0d80bde349dac
MaksZiem <mtziembicki@gmail.com>
2025-12-28 03:13:16 +0100

    day9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,33 @@
+from art import logo
+print(logo)
+
 # TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+
+def highest_bid(bids):
+    win_big = 0
+    winner = ""
+    for bid in bids:
+        print(bid)
+        if bids[bid] > win_big:
+            winner = bid
+            win_big = bids[bid]
+
+    print("winner: ", winner)
+    print("win bid: ", win_big)
+    print(bids)
+
+bids_ends = False
+bids = {}
+while bids_ends == False:
+    name = input("what is your name?: ")
+    bid =  int(input("What is your bid?: $"))
+    bids[name] = bid
+    end = input("End?")
+
+    if end == "tak":
+        highest_bid(bids)
+        bids_ends = True
+
+
 
 
